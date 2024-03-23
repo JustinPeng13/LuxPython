5afd1909e91ca1f97fe8ed444079be1ef8a41e4f
aureliony <39163684+aureliony@users.noreply.github.com>
2024-03-24 04:06:33 +0800

    Use LuxEnvironment to simulate game

--- agents/random_agent/main.py
+++ agents/random_agent/main.py
@@ -1,22 +1,45 @@
-from luxai2021.game.game import Game
-from luxai2021.game.actions import *
+import random
+from luxai2021.env.lux_env import LuxEnvironment
 from luxai2021.game.constants import LuxMatchConfigs_Default
 
+from agent import RandomAgent
+
+# if __name__ == "__main__":
+#     random_agent = RandomAgent()
+#     game = Game(configs=LuxMatchConfigs_Default, agents=[random_agent])
+#     # game = Game(configs=LuxMatchConfigs_Default)
+#     game.configs["seed"] = 0
+#     game.start_replay_logging()
+
+#     game_over = False
+#     while not game_over:
+#         print("Turn %i" % game.state["turn"])
+
+#         # Array of actions for both teams. Eg: MoveAction(team, unit_id, direction)
+#         actions = []
+#         # actions = agent(observation, None) # configuration = None
+#         game_over = game.run_turn_with_actions(actions)
+
+#     print("Game done, final map:")
+#     print(game.map.get_map_string())
+
+
 if __name__ == "__main__":
-    # Create a game
+    player = RandomAgent()
+    opponent = RandomAgent()
+
+    # Create a game environment
     configs = LuxMatchConfigs_Default
-    game = Game(configs)
-    game.configs["seed"] = 0
-    game.start_replay_logging(stateful=True)
+    configs["seed"] = random.randint(1, 100)
+    env = LuxEnvironment(configs=configs,
+                         learning_agent=player,
+                         opponent_agent=opponent)
 
-    game_over = False
-    while not game_over:
-        print("Turn %i" % game.state["turn"])
+    env.game.start_replay_logging()
+    is_game_error = env.run_no_learn()
 
-        # Array of actions for both teams. Eg: MoveAction(team, unit_id, direction)
-        actions = [] 
-
-        game_over = game.run_turn_with_actions(actions)
-
-    print("Game done, final map:")
-    print(game.map.get_map_string())
+    if not is_game_error:
+        print(f"Game done turn {env.game.state['turn']}, final map:")
+        print(env.game.map.get_map_string())
+    else:
+        raise Exception("Game error")
